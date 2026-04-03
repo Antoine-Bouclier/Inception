de8446a3739d48fb4a5e3417e6e47463778f9936
nanderyu <antoinebouclier1@gmail.com>
2026-04-03 07:35:15 -0400

    Mariadb: Done. (I think)

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 # Save password from secrets
-USER_PWD=$(cat /run/secrets/db_password.txt)
-USER_ROOT_PWD=$(cat /run/secrets/db_root_password.txt)
+USER_PWD=$(cat /run/secrets/db_password)
+USER_ROOT_PWD=$(cat /run/secrets/db_root_password)
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "First start: Initializing MariaDB."
@@ -34,4 +34,5 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 fi
 
 echo "MariaDB is ready."
-exec mariadbd-safe --datadir=/var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+exec mariadbd --user=mysql --datadir=/var/lib/mysql --console
